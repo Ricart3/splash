7690908bd6e8b4e032da15b7a1d814cea8d889ec
Hector Ricarte <hricarte11@gmail.com>
2023-07-06 18:56:09 -0600

    Agregando el proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'splash',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
